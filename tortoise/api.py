8ea6f34edd68b163abb7c38d6cde3e5e176e8069
fitbyprabhat <68177091+fitbyprabhat@users.noreply.github.com>
2022-10-04 00:12:52 +0530

    Update api.py

--- tortoise/api.py
+++ tortoise/api.py
@@ -281,6 +281,7 @@ class TextToSpeech:
         # Presets are defined here.
         presets = {
             'ultra_fast': {'num_autoregressive_samples': 16, 'diffusion_iterations': 30, 'cond_free': False},
+            'ultra_long': {'num_autoregressive_samples': 16, 'diffusion_iterations': 2000, 'cond_free': False},
             'fast': {'num_autoregressive_samples': 96, 'diffusion_iterations': 80},
             'standard': {'num_autoregressive_samples': 256, 'diffusion_iterations': 200},
             'high_quality': {'num_autoregressive_samples': 256, 'diffusion_iterations': 400},
